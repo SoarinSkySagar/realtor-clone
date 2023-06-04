483da6a8d82a92d7316065fbd3d248c4894403f9
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-06-05 02:31:04 +0530

    route and pages update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+
 function App() {
   return (
-    <h1 className="text-3xl bg-blue-400">Hello, World!</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/login" element={<SignIn/>}/>
+        <Route path="/register" element={<SignUp/>}/>
+        <Route path="/resetpwd" element={<ForgotPassword/>}/>
+        <Route path="/offers" element={<Offers/>}/>
+      </Routes>
+    </Router>
   );
 }
 
