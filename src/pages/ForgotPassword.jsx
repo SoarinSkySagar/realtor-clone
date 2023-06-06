7a506aa207037e8427ac4c8dbe2b28b1d7f0a8e3
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-06-06 18:27:14 +0530

    resetpwd function complete

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 export default function ForgotPassword() {
 
@@ -8,6 +10,20 @@ export default function ForgotPassword() {
   
   function onChange(e) {
     setEmail(e.target.value);
+  }
+
+  const navigate = useNavigate();
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Password reset email sent");
+    } catch (error) {
+      toast.error("No account exists with the given email");
+    }
+
   }
 
   return (
@@ -18,7 +34,7 @@ export default function ForgotPassword() {
           <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=773&q=80" alt="keys" className='rounded-3xl w-full' />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form>
+          <form onSubmit={onSubmit}>
             <input type="email" className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' id='email' value={email} onChange={onChange} placeholder='Email address'/>
             <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6'>
               <p>Don't have an account? <Link to="/register" className='text-red-600 hover:text-red-700 transition duration-200 ease-in-out'>Register</Link></p>
